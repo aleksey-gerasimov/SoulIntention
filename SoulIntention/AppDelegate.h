1ae7f0e6e2f279925e543b46b077f12c9d64918c
Aleksey <aleksey@Alekseys-Mac.local>
2014-11-18 08:13:46 +0200

    Initial Commit

---
+++ SoulIntention/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoulIntention
+//
+//  Created by Aleksey on 11/18/14.
+//  Copyright (c) 2014 ThinkMobiles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SoulIntention/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoulIntention
+//
+//  Created by Aleksey on 11/18/14.
+//  Copyright (c) 2014 ThinkMobiles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
